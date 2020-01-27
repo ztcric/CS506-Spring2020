8c712ae165c63f1936d7c3e98db5cfe58e0a343f
Cliff <tzcliff@outlook.com>
2020-01-27 17:40:05 -0500

    Polished Tree
    tree - removed useless var
    
    Update 01-python/infrastructure/tree.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Update 01-python/infrastructure/tree.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Update 01-python/infrastructure/tree.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Adding Comments
    
    Squash commits

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,22 @@
-def draw_tree():
-    print("tree not found")
+def draw_tree(height = 5):
+    """
+        Draw a tree
+        Parameters:
+        height (int): trunk height of the tree
+        Returns: void
+    """
+    for level in range(2*height):
+        """Drawing tree crown"""
+        for _ in range(2*height-level):
+            print(" ", end="")
+        for _ in range(level):
+            print("--", end="")
+        print()
+
+    for level in range(height):
+        """Drawing tree trunk"""
+        for _ in range(2*height - 2):
+            print(" ",end="")
+        print("||||")
     return
+

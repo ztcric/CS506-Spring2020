c74a1cb3e729d65419118b3fd9a42e81634caf1c
lux <lindengone97@gmail.com>
2020-01-28 09:59:40 -0500

    adding a museum to the city
    adding a museum to the city
    
    adding a museum into the city

--- 01-python/leisure/museum.py
+++ 01-python/leisure/museum.py
@@ -1,3 +1,21 @@
-def draw_museum():
-    print("museum not found")
-    return
+def draw_museum(height = 3):
+	"""
+	Draw a museum with specified height.
+
+	Parameters:
+	height(int): the height of the pillars of the museum.
+
+	Returns:
+	None
+
+	"""
+
+	print("   ==============")
+	print("  =              =")
+	print(" =                = ")
+	print("=                  =")
+	print("====================")
+	for _ in range(height):
+		print("|| || || || || || ||")
+	print("====================")
+	return
